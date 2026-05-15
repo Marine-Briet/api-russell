bf8a8721e9f8e6d78a4d98da4980c6b26f857e35
Marine-Briet <marinebriet@hotmail.com>
2026-05-15 13:31:46 +0800

    connexion MongoDB

--- server/app.js
+++ server/app.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+const mongodb = require('./db/mongo');
 
+mongodb.initClientDbConnection();
+app.use(cors({
+    exposeHeaders: ['Authorization'],
+    origin: '*'
+}));
+
+app.use(express.json());
 module.exports = app
