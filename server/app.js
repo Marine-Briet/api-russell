fb6d13737a39757a706fd0c89865e76be57835d3
Marine-Briet <marinebriet@hotmail.com>
2026-05-15 14:59:14 +0800

    connexion des routes à routes/index.js et ce dernier avec app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const mongodb = require('./db/mongo');
+const indexRouter = require('./routes/index');
 
 mongodb.initClientDbConnection();
 app.use(cors({
@@ -10,5 +11,6 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use('/', indexRouter);
 
 module.exports = app
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,8 @@ const userRoute = require('../routes/users');
 const catwaysRoute = require('../routes/catways');
 const reservationsRoute = require('../routes/reservations');
 
+
+
 router.get('/', async (req, res) => {
     res.status(200).json({ message: 'Bienvenue sur l\'API Russell!' })
 });
